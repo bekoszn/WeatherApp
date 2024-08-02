16ce6316780d46c1ed6e20e974c4529b6cb1f9ab
bekoszn <berkeozguder@gmail.com>
2024-08-02 19:24:16 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Berke Özgüder on 2.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
